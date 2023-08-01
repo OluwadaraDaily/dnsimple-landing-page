6c1a2bc27560348336fab1b95860d16bfbb6a33d
Oluwadara Oloye <daraoloye99@gmail.com>
2023-08-01 19:22:58 +0100

    Implement footer with designs

---
+++ app/javascript/components/footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="bg-yellow pa4 flex justify-between">
+    <div class="ml5">
+      <img src="banana.svg" alt="Logo of BananaDNS" width="35">
+      <p class="fw7 f4 lh-title mt2 mb3">BananaDNS</p>
+      <p class="f5 fw3">We're bananas about DNS since 2010!</p>
+    </div>
+    <div class="tr">
+      <p class="fw3 f5 mb2">About Us</p>
+      <p class="fw3 f5 mb2">The Company</p>
+      <p class="fw3 f5 mb2">Privacy Policies</p>
+      <p class="fw3 f5 mb2">I hope this was fun</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -3,11 +3,13 @@ import initVue from '../components/init-vue.js';
 import AppButton from '../components/app-button.vue';
 import Header from '../components/header.vue'
 import HSubSection from '../components/horizontal-subsection.vue'
+import Footer from '../components/footer.vue'
 
 const components = {
   'app-button': AppButton,
   'app-header': Header,
-  'h-subsection': HSubSection
+  'h-subsection': HSubSection,
+  'app-footer': Footer
 };
 
 initVue(components);
