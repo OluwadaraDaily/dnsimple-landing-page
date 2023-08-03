5c57da49056186f1e6aca47537add2d177995f19
Oluwadara Oloye <daraoloye99@gmail.com>
2023-08-03 14:41:12 +0100

    Add more semantic markup

--- app/javascript/components/footer.vue
+++ app/javascript/components/footer.vue
@@ -2,15 +2,15 @@
   <footer class="bg-yellow pa4 flex justify-between">
     <div class="ml5">
       <img src="banana.svg" alt="Logo of BananaDNS" width="35">
-      <p class="fw7 f4 lh-title mt2 mb3">BananaDNS</p>
+      <h1 class="fw7 f4 lh-title mt2 mb3">BananaDNS</h1>
       <p class="f5 fw3">We're bananas about DNS since 2010!</p>
     </div>
-    <div class="tr">
-      <p class="fw3 f5 mb2">About Us</p>
-      <p class="fw3 f5 mb2">The Company</p>
-      <p class="fw3 f5 mb2">Privacy Policies</p>
-      <p class="fw3 f5 mb2">I hope this was fun</p>
-    </div>
+    <ul class="tr">
+      <li class="fw3 f5 mb2">About Us</li>
+      <li class="fw3 f5 mb2">The Company</li>
+      <li class="fw3 f5 mb2">Privacy Policies</li>
+      <li class="fw3 f5 mb2">I hope this was fun</li>
+    </ul>
   </footer>
 </template>
 
--- app/javascript/components/header.vue
+++ app/javascript/components/header.vue
@@ -2,7 +2,7 @@
   <header class="pa3 bg-yellow">
       <div class="flex items-center ml4">
         <img src="banana.svg" alt="Logo of BananaDNS" width="35" class="mr2">
-        <p class="fw7 f4 lh-title">BananaDNS</p>
+        <h1 class="fw7 f4 lh-title">BananaDNS</h1>
       </div>
   </header>
 </template>
